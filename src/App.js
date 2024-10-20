f1057157a0ea67238c6941c36d2f9a6e7f282683
Nathaniel Lee <nathaniellee@nathaniels-air.lan>
2024-10-19 23:19:42 -0400

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      5eSRD
+    </header>
+    <div className="App-main">
+      <p>Hello, world!</p>
+    </div>
+  </div>
+);
+
+export default App;
