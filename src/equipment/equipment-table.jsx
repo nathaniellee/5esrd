97980e2860d9b112e5d02d9ce32d0f3e0b37242d
Nathaniel Lee <nathaniel.k.lee@gmail.com>
2024-11-03 15:50:12 -0500

    Updated the equipment component to include a new equipment table component.

---
+++ src/equipment/equipment-table.jsx
@@ -0,0 +1,154 @@
+import PropTypes from 'prop-types';
+import React, { useCallback, useState } from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHeader,
+  TableHeaderCell,
+  TableRow,
+  Title1,
+  makeStyles,
+  tokens,
+} from '@fluentui/react-components';
+import { Paginator } from '../common/paginator';
+import { equipmentCategories } from '../constants';
+
+const useStyles = makeStyles({
+  th: {
+    fontWeight: tokens.fontWeightBold,
+  },
+});
+
+const columns = [
+  { columnKey: 'name', label: 'Name' },
+  { columnKey: 'cost', label: 'Cost' },
+  { columnKey: 'weight', label: 'Weight' },
+  { columnKey: 'equipmentCategory', label: 'Category' },
+];
+const sortableColumns = [
+  'name',
+  'weight',
+  'equipmentCategory',
+];
+const isSortableColumn = columnKey => sortableColumns.includes(columnKey);
+
+const getWeightString = (weight) => {
+  if (!weight || Number.isNaN(weight)) {
+    return '--';
+  }
+  if (weight === 1) {
+    return '1 lb.';
+  }
+  
+  const integer = Number.parseInt(weight);
+  const decimal = weight - integer;
+
+  if (decimal === 0.25) {
+    return integer === 0
+      ? '1/4 lbs.'
+      : `${integer} 1/4 lbs.`;
+  }
+  if (decimal === 0.5) {
+    return integer === 0
+      ? '1/2 lbs.'
+      : `${integer} 1/2 lbs.`;
+  }
+
+  return `${weight} lbs.`;
+};
+
+export const EquipmentTable = ({
+  equipments,
+  onChangeSort,
+  onNextPage,
+  onPrevPage,
+  pageNumber,
+  perPage,
+  totalEquipmentCount = 0,
+}) => {
+  const styles = useStyles();
+
+  const [sortDirection, setSortDirection] = useState('ascending');
+  const [sortedColumn, setSortedColumn] = useState(sortableColumns[0]);
+
+  const onClickTableHeaderCell = useCallback((columnKey) => {
+    if (columnKey !== sortedColumn) {
+      setSortedColumn(columnKey);
+      setSortDirection('ascending');
+      onChangeSort({
+        columnKey,
+        sortDirection: 'ascending',
+      });
+    } else {
+      const newSortDirection = sortDirection === 'ascending' ? 'descending' : 'ascending';
+      setSortDirection(newSortDirection);
+      onChangeSort({
+        columnKey,
+        sortDirection: newSortDirection,
+      })
+    }
+  }, [sortDirection, sortedColumn]);
+
+  return (
+    <div>
+      <Title1>Equipment</Title1>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            {columns.map(({ columnKey, label }) => (
+              <TableHeaderCell
+                key={columnKey}
+                className={styles.th}
+                onClick={() => {
+                  if (isSortableColumn(columnKey)) {
+                    onClickTableHeaderCell(columnKey);
+                  }
+                }}
+                sortDirection={sortedColumn === columnKey ? sortDirection : undefined}
+                sortable={isSortableColumn(columnKey)}
+              >
+                {label}
+              </TableHeaderCell>
+            ))}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {equipments.map(equipment => (
+            <TableRow key={equipment.id}>
+              <TableCell>{equipment.name}</TableCell>
+              <TableCell>{`${equipment.cost.quantity} ${equipment.cost.unit}`}</TableCell>
+              <TableCell>{getWeightString(equipment.weight)}</TableCell>
+              <TableCell>{equipmentCategories[equipment.equipmentCategory]}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <Paginator
+        onClickNext={onNextPage}
+        onClickPrev={onPrevPage}
+        pageNumber={pageNumber}
+        perPage={perPage}
+        totalCount={totalEquipmentCount}
+      />
+    </div>
+  );
+};
+
+EquipmentTable.propTypes = {
+  equipments: PropTypes.arrayOf(PropTypes.shape({
+    cost: PropTypes.shape({
+      quantity: PropTypes.number.isRequired,
+      unit: PropTypes.string.isRequired,
+    }).isRequired,
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    weight: PropTypes.number,
+  })).isRequired,
+  onChangeSort: PropTypes.func.isRequired,
+  onNextPage: PropTypes.func.isRequired,
+  onPrevPage: PropTypes.func.isRequired,
+  pageNumber: PropTypes.number.isRequired,
+  perPage: PropTypes.number.isRequired,
+  totalEquipmentCount: PropTypes.number,
+};
--- src/equipment/equipment.jsx
+++ src/equipment/equipment.jsx
@@ -1,10 +1,90 @@
-import React from 'react';
-import { Title1 } from '@fluentui/react-components';
+import React, { useCallback, useEffect, useState } from 'react';
+import {
+  DEFAULT_ORDER,
+  DEFAULT_PAGE_NUMBER,
+  DEFAULT_PER_PAGE,
+} from '../api/constants';
+import {
+  fetchEquipments,
+  fetchTotalEquipmentCount,
+  transformEquipment,
+} from '../api/fetchEquipments';
+import { EquipmentTable } from './equipment-table';
 
 export const Equipment = () => {
+  const [equipments, setEquipments] = useState([]);
+  const [order, setOrder] = useState(DEFAULT_ORDER);
+  const [pageNumber, setPageNumber] = useState(DEFAULT_PAGE_NUMBER);
+  const [totalEquipmentCount, setTotalEquipmentCount] = useState(0);
+
+  const perPage = DEFAULT_PER_PAGE;
+
+  const onChangeSort = useCallback(({ columnKey, sortDirection }) => {
+    const newOrder = [{
+      by: columnKey,
+      direction: sortDirection,
+    }];
+
+    if (columnKey !== 'name') {
+      newOrder.push({
+        by: 'name',
+        direction: sortDirection,
+      });
+    }
+    setOrder(newOrder);
+    setPageNumber(DEFAULT_PAGE_NUMBER);
+    fetchEquipments({
+      order: newOrder,
+      pageNumber: DEFAULT_PAGE_NUMBER,
+      perPage,
+    }).then(onDoneFetchEquipments);
+  });
+
+  const onDoneFetchEquipments = useCallback((equipments) => {
+    setEquipments(equipments.map(transformEquipment));
+  }, []);
+
+  const onNextPage = useCallback(() => {
+    const newPageNumber = pageNumber + 1;
+    setPageNumber(newPageNumber);
+    fetchEquipments({
+      order,
+      pageNumber: newPageNumber,
+      perPage,
+    }).then(onDoneFetchEquipments);
+  }, [order, pageNumber, perPage]);
+
+  const onPrevPage = useCallback(() => {
+    const newPageNumber = pageNumber - 1;
+    setPageNumber(newPageNumber);
+    fetchEquipments({
+      order,
+      pageNumber: newPageNumber,
+      perPage,
+    }).then(onDoneFetchEquipments);
+  }, [order, pageNumber, perPage]);
+
+  useEffect(() => {
+    fetchTotalEquipmentCount().then((count) => {
+      setTotalEquipmentCount(count);
+      fetchEquipments({
+        pageNumber: DEFAULT_PAGE_NUMBER,
+        perPage,
+      }).then(onDoneFetchEquipments);
+    });
+  }, []);
+
   return (
     <div className="Equipment">
-      <Title1>Equipment</Title1>
+      <EquipmentTable
+        equipments={equipments}
+        onChangeSort={onChangeSort}
+        onNextPage={onNextPage}
+        onPrevPage={onPrevPage}
+        pageNumber={pageNumber}
+        perPage={perPage}
+        totalEquipmentCount={totalEquipmentCount}
+      />
     </div>
   );
 };
